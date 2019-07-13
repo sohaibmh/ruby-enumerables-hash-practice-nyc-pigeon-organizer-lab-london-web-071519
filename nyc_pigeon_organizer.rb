88837856bd80eb6b4e62d7ac596075e4a30a584f
Sohaib Mubashir <sohaibmh@yahoo.com>
2019-07-13 10:50:36 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |key,values|
+    values.each do |value,names|
+      names.each do |name|
+        pigeon_list[name] ||= {}
+        pigeon_list[name][key] ||= []
+        pigeon_list[name][key] << value.to_s
+      end
+    end
+  end
+  pigeon_list
 end
